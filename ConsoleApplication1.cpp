1488b1d87d52c6e58727bc548e1c8c04d3a6637b
Eloy David Alata Giraldo <112296490+EloyAlata2002@users.noreply.github.com>
2023-02-02 09:00:05 -0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+// ConsoleApplication1.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "conio.h"
+#include "Manejador.h"
+
+using namespace std;
+using namespace System;
+//using namespace Sys;
+
+int main (void)
+{
+
+	char tecla;
+	CManejador objman;
+	while (1)
+	{	if (kbhit())
+		{	tecla = toupper(_getch());
+		    if (tecla == 'C' || tecla == 'T')
+			    objman.Insertar(tecla);		   
+			if (tecla == 'H')	objman.Cambiar_Direccion_Horizontal();
+			if (tecla == 'V')	objman.Cambiar_Direccion_Vertical();
+			if (tecla == 'R')	objman.Cambiar_Relleno();
+		}
+		objman.Dibujar();
+		_sleep(50);
+		objman.Borrar();
+		objman.Mover();
+	}
+
+
+_getch();
+    return 0;
+
+}
+
+
